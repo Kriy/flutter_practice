b85f9e450bc862a5dc7343036aa5a8ed59e4ee56
dufan <hj2drf@163.com>
2019-09-13 21:11:04 +0800

    添加搜索界面

--- lib/home/search_page.dart
+++ lib/home/search_page.dart
@@ -80,6 +80,197 @@ class SearchPageState extends State<SearchPage> {
                     margin: const EdgeInsets.only(left: 16.0, bottom: 16.0),
                     alignment: Alignment.topLeft,
                   ),
+                  Container(
+                    child: Chip(
+                      label: FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          '李彦宏传闻辟谣',
+                          style: TextStyle(color: GlobalConfig.fontColor),
+                        ),
+                      ),
+                      backgroundColor: GlobalConfig.dark == true
+                          ? Colors.white10
+                          : Colors.black12,
+                    ),
+                    margin: const EdgeInsets.only(left: 16.0, bottom: 16.0),
+                    alignment: Alignment.topLeft,
+                  ),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    child: Chip(
+                      label: FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          '小米8',
+                          style: TextStyle(color: GlobalConfig.fontColor),
+                        ),
+                      ),
+                      backgroundColor: GlobalConfig.dark == true
+                          ? Colors.white10
+                          : Colors.black12,
+                    ),
+                    margin: const EdgeInsets.only(left: 16.0, bottom: 16.0),
+                    alignment: Alignment.topLeft,
+                  ),
+                  Container(
+                    child: Chip(
+                      label: FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          '超时空同居',
+                          style: TextStyle(color: GlobalConfig.fontColor),
+                        ),
+                      ),
+                      backgroundColor: GlobalConfig.dark == true
+                          ? Colors.white10
+                          : Colors.black12,
+                    ),
+                    margin: const EdgeInsets.only(left: 16.0, bottom: 16.0),
+                    alignment: Alignment.topLeft,
+                  ),
+                  Container(
+                    child: Text(
+                      '搜索历史',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    margin: const EdgeInsets.only(left: 16.0, bottom: 16.0),
+                    alignment: Alignment.topLeft,
+                  ),
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          child: Icon(
+                            Icons.access_time,
+                            color: GlobalConfig.fontColor,
+                            size: 16.0,
+                          ),
+                          margin: const EdgeInsets.only(right: 12.0),
+                        ),
+                        Expanded(
+                          child: Container(
+                            child: Text(
+                              '业余兴趣',
+                              style: TextStyle(
+                                  color: GlobalConfig.fontColor,
+                                  fontSize: 14.0),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Icon(
+                            Icons.clear,
+                            color: GlobalConfig.fontColor,
+                            size: 16.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                    margin: const EdgeInsets.only(
+                        left: 16.0, right: 16.0, bottom: 10.0),
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    decoration: BoxDecoration(
+                      border: BorderDirectional(
+                        bottom: BorderSide(
+                            color: GlobalConfig.dark == true
+                                ? Colors.white12
+                                : Colors.black12),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          child: Icon(
+                            Icons.access_time,
+                            color: GlobalConfig.fontColor,
+                            size: 16.0,
+                          ),
+                          margin: const EdgeInsets.only(right: 12.0),
+                        ),
+                        Expanded(
+                          child: Container(
+                            child: Text(
+                              '三体',
+                              style: TextStyle(
+                                color: GlobalConfig.fontColor,
+                                fontSize: 14.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Icon(
+                            Icons.clear,
+                            color: GlobalConfig.fontColor,
+                            size: 16.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                    margin: const EdgeInsets.only(
+                        left: 16.0, right: 16.0, bottom: 10.0),
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    decoration: BoxDecoration(
+                      border: BorderDirectional(
+                        bottom: BorderSide(
+                            color: GlobalConfig.dark == true
+                                ? Colors.white12
+                                : Colors.black12),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          child: Icon(
+                            Icons.access_time,
+                            color: GlobalConfig.fontColor,
+                            size: 16.0,
+                          ),
+                          margin: const EdgeInsets.only(right: 12.0),
+                        ),
+                        Expanded(
+                          child: Container(
+                            child: Text(
+                              '人类未来',
+                              style: TextStyle(
+                                color: GlobalConfig.fontColor,
+                                fontSize: 14.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Icon(
+                            Icons.clear,
+                            color: GlobalConfig.fontColor,
+                            size: 16.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                    margin: const EdgeInsets.only(
+                        left: 16.0, right: 16.0, bottom: 10.0),
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    decoration: BoxDecoration(
+                      border: BorderDirectional(
+                        bottom: BorderSide(
+                            color: GlobalConfig.dark == true
+                                ? Colors.white12
+                                : Colors.black12),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ],
