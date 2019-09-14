e1901d16c634a7fcc49c5cbfd6abee6d9f8b089a
dufan <hj2drf@163.com>
2019-09-14 21:19:38 +0800

    change

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutter_zhihu/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(ZhiHu());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
