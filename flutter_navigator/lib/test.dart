90bf30bf55073a65c2f648f41a1f9391a1aff5d7
Kriy <hj2drf@163.com>
2019-09-17 14:26:44 +0800

    init

---
+++ flutter_navigator/lib/test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Test extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'title',
+      routes: <String, WidgetBuilder>{},
+    );
+  }
+}
