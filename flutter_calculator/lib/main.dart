05159463c05813c703c68f33cbf3efb893e4f0eb
Kriy <hj2drf@163.com>
2019-09-19 18:24:35 +0800

    添加计算器demo

--- flutter_calculator/lib/main.dart
+++ flutter_calculator/lib/main.dart
@@ -1,111 +1,206 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'number.dart';
+import 'operator.dart';
+import 'result.dart';
+
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return CupertinoApp(
+      title: 'Flutter Calculator',
+      debugShowCheckedModeBanner: false,
+      home: CalculatorPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class CalculatorPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => CalculatorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CalculatorState extends State<StatefulWidget> {
+  List<Result> results = [];
+  String currentDisplay = '0';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  onResultButtonPressed(display) {
+    if (results.length > 0) {
+      var result = results[results.length - 1];
+      if (display == '=') {
+        result.result = result.oper.calculate(
+            double.parse(result.firstNum), double.parse(result.secondNum));
+      } else if (display == '<<') {
+        results.removeLast();
+      }
+    }
+    pickCurrentDisplay();
+  }
+
+  onOperatorButtonPressed(Operator oper) {
+    if (results.length > 0) {
+      var result = results[results.length - 1];
+      if (result.result != null) {
+        var newRes = Result();
+        newRes.firstNum = currentDisplay;
+        newRes.oper = oper;
+        results.add(newRes);
+      } else if (result.firstNum != null) {
+        result.oper = oper;
+      }
+    }
+    pickCurrentDisplay();
+  }
+
+  onNumberButtonPressed(Number number) {
+    var result = results.length > 0 ? results[results.length - 1] : Result();
+    if (result.firstNum == null || result.oper == null) {
+      result.firstNum = number.apply(currentDisplay);
+    } else if (result.result == null) {
+      if (result.secondNum == null) {
+        currentDisplay = '0';
+      }
+      result.secondNum = number.apply(currentDisplay);
+    } else {
+      var newRes = Result();
+      currentDisplay = '0';
+      newRes.firstNum = number.apply(currentDisplay);
+      results.add(newRes);
+    }
+    if (results.length == 0) {
+      results.add(result);
+    }
+    pickCurrentDisplay();
+  }
+
+  pickCurrentDisplay() {
+    this.setState(() {
+      var display = '0';
+      results.removeWhere((item) =>
+      item.firstNum == null && item.oper == null && item.secondNum == null);
+      if (results.length > 0) {
+        var result = results[results.length - 1];
+        if (result.result != null) {
+          display = format(result.result);
+        } else if (result.secondNum != null && result.oper != null) {
+          display = result.secondNum;
+        } else if (result.firstNum != null) {
+          display = result.firstNum;
+        }
+      }
+      currentDisplay = display;
     });
+  }
+
+  String format(num number) {
+    if (number == number.toInt()) {
+      return number.toInt().toString();
+    }
+    return number.toString();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return CupertinoPageScaffold(
+      child: Container(
+          color: Colors.grey[100],
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                key: Key('Current_Display'),
+                flex: 2,
+                child: FractionallySizedBox(
+                  widthFactor: 1.0,
+                  heightFactor: 1.0,
+                  child: Container(
+                    color: Colors.lightBlue[300],
+                    alignment: Alignment.bottomRight,
+                    padding: const EdgeInsets.all(16.0),
+                    child: ResultDisplay(result: currentDisplay),
+                  ),
+                ),
+              ),
+              Expanded(
+                  key: Key('History_Display'),
+                  child: FractionallySizedBox(
+                      widthFactor: 1.0,
+                      heightFactor: 1.0,
+                      child: Container(
+                        color: Colors.black54,
+                        child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            reverse: true,
+                            // mainAxisAlignment: MainAxisAlignment.end,
+                            children: results.reversed.map((result) {
+                              return HistoryBlock(result: result);
+                            }).toList()),
+                      )),
+                  flex: 1),
+              Expanded(
+                  key: Key('Number_Button_Line_1'),
+                  child: NumberButtonLine(
+                    array: [
+                      NormalNumber('1'),
+                      NormalNumber('2'),
+                      NormalNumber('3')
+                    ],
+                    onPress: onNumberButtonPressed,
+                  ),
+                  flex: 1),
+              Expanded(
+                  key: Key('Number_Button_Line_2'),
+                  child: NumberButtonLine(
+                    array: [
+                      NormalNumber('4'),
+                      NormalNumber('5'),
+                      NormalNumber('6')
+                    ],
+                    onPress: onNumberButtonPressed,
+                  ),
+                  flex: 1),
+              Expanded(
+                  key: Key('Number_Button_Line_3'),
+                  child: NumberButtonLine(
+                    array: [
+                      NormalNumber('7'),
+                      NormalNumber('8'),
+                      NormalNumber('9')
+                    ],
+                    onPress: onNumberButtonPressed,
+                  ),
+                  flex: 1),
+              Expanded(
+                  key: Key('Number_Button_Line_4'),
+                  child: NumberButtonLine(
+                    array: [SymbolNumber(), NormalNumber('0'), DecimalNumber()],
+                    onPress: onNumberButtonPressed,
+                  ),
+                  flex: 1),
+              Expanded(
+                  key: Key('Operator_Group'),
+                  child: OperatorGroup(onOperatorButtonPressed),
+                  flex: 1),
+              Expanded(
+                  key: Key('Result_Button_Area'),
+                  child: Row(
+                    children: <Widget>[
+                      ResultButton(
+                        display: '<<',
+                        color: Colors.red,
+                        onPress: onResultButtonPressed,
+                      ),
+                      ResultButton(
+                          display: '=',
+                          color: Colors.green,
+                          onPress: onResultButtonPressed),
+                    ],
+                  ),
+                  flex: 1)
+            ],
+          )),
     );
   }
 }
--- flutter_calculator/lib/operator.dart
+++ flutter_calculator/lib/operator.dart
@@ -37,6 +37,96 @@ class SubOperator extends Operator {
   }
 }
 
-class MultiOperator extends Operator{
+class MultiOperator extends Operator {
+  @override
+  String get display => 'X';
 
+  @override
+  Color get color => Colors.lightBlue[300];
+
+  @override
+  num calculate(num first, num second) {
+    return first / second;
+  }
+}
+
+class DivisionOperator extends Operator {
+  @override
+  String get display => '+';
+
+  @override
+  Color get color => Colors.purple[300];
+
+  @override
+  num calculate(num first, num second) {
+    return first / second;
+  }
+}
+
+class OperatorGroup extends StatelessWidget {
+  final PressOperationCallback onOperatorButtonPressed;
+
+  OperatorGroup(this.onOperatorButtonPressed);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[],
+    );
+  }
+}
+
+class OperatorButton extends StatefulWidget {
+  final Operator oper;
+  final PressOperationCallback onPress;
+
+  OperatorButton({@required this.oper, this.onPress})
+      : assert(Operator != null);
+
+  @override
+  State<StatefulWidget> createState() => OperatorButtonState();
+}
+
+class OperatorButtonState extends State<OperatorButton> {
+  bool pressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: GestureDetector(
+          onTap: () {
+            if (widget.onPress != null) {
+              widget.onPress(widget.oper);
+              setState(() {
+                pressed = true;
+              });
+              Future.delayed(
+                  const Duration(milliseconds: 200),
+                  () => setState(() {
+                        pressed = false;
+                      }));
+            }
+          },
+          child: Container(
+            alignment: Alignment.centerRight,
+            decoration: BoxDecoration(
+              color: pressed
+                  ? Color.alphaBlend(Colors.white30, widget.oper.color)
+                  : widget.oper.color,
+            ),
+            child: Text(
+              '${widget.oper.display}',
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
