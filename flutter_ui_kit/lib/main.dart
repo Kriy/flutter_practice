0bcafecd15e84e81a658b3de1280591ea71128ec
dufan <hj2drf@163.com>
2020-08-12 16:47:46 +0800

    add

--- flutter_ui_kit/lib/main.dart
+++ flutter_ui_kit/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/di/dependency_injection.dart';
+import 'package:flutterapp/myapp.dart';
 
 void main() {
   Injector.configure(Flavor.MOCK);
